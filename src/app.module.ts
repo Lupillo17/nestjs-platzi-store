607a42769d6abfef32cf953a2be1a290bc759982
Lupillo17 <joseguadaluperivera17@gmail.com>
2022-03-10 13:36:09 -0600

    Validaciones con class-validator y mapped-types, adiciones se agrego un ValidationPipe al main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,10 @@ import { CustomersController } from './controllers/customers/customers.controlle
 import { OrdersController } from './controllers/orders/orders.controller';
 import { BrandsController } from './controllers/brands/brands.controller';
 import { ProductsService } from './services/products/products.service';
+import { BrandsService } from './services/brands/brands.service';
+import { CategoriesService } from './services/categories/categories.service';
+import { CustomersService } from './services/customers/customers.service';
+import { OrdersService } from './services/orders/orders.service';
 
 @Module({
   imports: [],
@@ -18,6 +22,13 @@ import { ProductsService } from './services/products/products.service';
     OrdersController,
     BrandsController,
   ],
-  providers: [AppService, ProductsService],
+  providers: [
+    AppService,
+    ProductsService,
+    BrandsService,
+    CategoriesService,
+    CustomersService,
+    OrdersService,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      /*whitelist : true => Si los atributos que se mandan en el Request
+        no están en el DTO, los descarta para evitar
+        inserción de datos maliciosos o datos que no
+        están contemplados en la BD */
+      whitelist: true,
+      // enviar alerta de datos no contemplados en BD
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
