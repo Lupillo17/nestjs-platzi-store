9fe8307e4a85a7b8fc8144bd9674c49ddc1c6808
Lupillo17 <joseguadaluperivera17@gmail.com>
2022-03-14 15:54:08 -0600

    Validacion de variables de entorno con Joi

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { ConfigService, ConfigType } from '@nestjs/config';
+import { ConfigType } from '@nestjs/config';
 import config from './config';
 
 @Injectable()
