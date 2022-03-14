122e7f674967ffdc45a1dec4ef5639bf4661d31d
Lupillo17 <joseguadaluperivera17@gmail.com>
2022-03-14 14:31:17 -0600

    Gloab Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
-
-const API_KEY = '1234567';
-const API_KEY_PROD = 'PROD123312';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [HttpModule, UsersModule, ProductsModule],
+  imports: [HttpModule, UsersModule, ProductsModule, DatabaseModule],
   controllers: [AppController],
   providers: [
     AppService,
-    {
-      provide: 'API_KEY',
-      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
-    },
     {
       provide: 'TASK',
       useFactory: async (http: HttpService) => {
