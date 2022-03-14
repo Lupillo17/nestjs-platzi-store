0ef3781642f31347ece100ab71069dd838d1489f
Lupillo17 <joseguadaluperivera17@gmail.com>
2022-03-14 13:02:55 -0600

    useFactory => async y await

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { HttpModule, HttpService, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -8,13 +8,23 @@ const API_KEY = '1234567';
 const API_KEY_PROD = 'PROD123312';
 
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [HttpModule, UsersModule, ProductsModule],
   controllers: [AppController],
   providers: [
     AppService,
     {
       provide: 'API_KEY',
       useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
+    },
+    {
+      provide: 'TASK',
+      useFactory: async (http: HttpService) => {
+        const taks = await http
+          .get('https://jsonplaceholder.typicode.com/todos')
+          .toPromise();
+        return taks.data;
+      },
+      inject: [HttpService],
     },
   ],
 })
