f22409b01299c381865a6fb9c2a33f8db30df98e
Lupillo17 <joseguadaluperivera17@gmail.com>
2022-03-14 15:39:44 -0600

    Tipado en config

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,13 @@ import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { enviroments } from './enviroments';
+import config from './config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: enviroments[process.env.NODE_ENV] || '.env',
+      load: [config],
       isGlobal: true,
     }),
     HttpModule,
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,16 +1,17 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { ConfigService, ConfigType } from '@nestjs/config';
+import config from './config';
 
 @Injectable()
 export class AppService {
   constructor(
     @Inject('API_KEY') private apiKey: string,
     @Inject('TASK') private task: any[],
-    private configService: ConfigService,
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
   ) {}
   getHello(): string {
-    const apiKey = this.configService.get<number>('API_KEY');
-    const dbName = this.configService.get('DATABASE_NAME');
+    const apiKey = this.configService.apikey;
+    const dbName = this.configService.database.name;
     return `Hello World! ${apiKey}, ${dbName}`;
   }
 }
